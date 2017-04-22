d0fb1793f28efb1297813b8aa07859c72c02bda9
smb564 <supun.14@cse.mrt.ac.lk>
2017-04-22 18:28:42 +0530

    Updated company admin detail views

--- src/main/webapp/app/entities/company-admin/company-admin-detail.controller.js
+++ src/main/webapp/app/entities/company-admin/company-admin-detail.controller.js
@@ -5,17 +5,29 @@
         .module('pulsebeatApp')
         .controller('CompanyAdminDetailController', CompanyAdminDetailController);
 
-    CompanyAdminDetailController.$inject = ['$scope', '$rootScope', '$stateParams', 'previousState', 'entity', 'CompanyAdmin'];
+    CompanyAdminDetailController.$inject = ['$scope', '$rootScope', '$stateParams', 'previousState', 'entity', 'CompanyAdmin', 'User', 'Company'];
 
-    function CompanyAdminDetailController($scope, $rootScope, $stateParams, previousState, entity, CompanyAdmin) {
+    function CompanyAdminDetailController($scope, $rootScope, $stateParams, previousState, entity, CompanyAdmin, User, Company) {
         var vm = this;
 
         vm.companyAdmin = entity;
         vm.previousState = previousState.name;
+        getCompany(vm.companyAdmin.companyId);
+        getUserInfo(vm.companyAdmin.userId);
 
         var unsubscribe = $rootScope.$on('pulsebeatApp:companyAdminUpdate', function(event, result) {
             vm.companyAdmin = result;
         });
+
+        // functions to get company and user details
+        function getCompany(companyId){
+            vm.company = Company.get({'id' : companyId});
+        }
+
+        function getUserInfo(userLogin){
+            vm.user = User.get({'login' : userLogin});
+        }
+
         $scope.$on('$destroy', unsubscribe);
     }
 })();
